63edd59b4cdc0ace76ad38b51a5cd65a93058c7b
lucasiqueira <33107504+lucasiqueira@users.noreply.github.com>
2017-11-07 23:19:36 -0200

    Outros

--- src/br/com/lucas/HelloWorld.java
+++ src/br/com/lucas/HelloWorld.java
@@ -3,6 +3,13 @@ package br.com.lucas;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		System.out.println("Hello World");
+		int a = 13;
+		int b = 2;
+		int c = a + b;
+		if(c>10) {
+			System.out.println("Maior que 10");
+		}
+		System.out.println("O resultado é: "+c);
 	} 
 }
